d4905a2e14dc2489cb4ef9b37ed53eb77781c4e5
Jose-Alvaro <84627188+Jose-Alvaro@users.noreply.github.com>
2021-08-14 17:46:29 +0200

    Add files via upload

---
+++ src/streamlit.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+
+st.set_page_config(layout="wide")
+
+url="http://127.0.0.1:5000/"
+operacion="get"
+query ="Island"
+st.write("Pinguinos!")
+
+islands["Torgersen","Biscoe","Dream","" ]
+value = st.radio('Island of...', islands)
+
+if value:
+    res = requests.get(f"{url}{operacion}?{query}={value}")
+    respuesta = res.json()
+    dfp = pd.DataFrame(respuesta)
+
+    dfp = dfp.drop(columns=["Sample Number","Individual ID", "Clutch Completion", "Date Egg", "studyName"])
+
+
+
+
+
+    st.write(dfp)
